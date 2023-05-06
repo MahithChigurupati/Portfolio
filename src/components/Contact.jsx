6906f8ac8dacde4179b9b5e466da1997067ca0d6
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2023-05-05 21:22:23 -0400

    env variables set for emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,10 +7,6 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
-//template_8ya2knp
-// service_u4ci3dd
-// mfcuIG3AoIjprY8i0
-
 const Contact = () => {
 
   const formRef = useRef();
@@ -38,19 +34,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        "service_u4ci3dd",
-        "template_8ya2knp",
-        // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID, 
-        // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID, 
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Mahith Chigurupati",
+          to_name: import.meta.env.VITE_APP_EMAILJS_TO_NAME,
           from_email: form.email,
-          to_email: "saimahith.chigurupati@gmail.com",
+          to_email: import.meta.env.VITE_APP_EMAILJS_TO_NAME,
           message: form.message,
         },
-        "mfcuIG3AoIjprY8i0"
-        // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
